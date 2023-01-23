f090d9a76709aa2d3c619a887b88fc11cff3a57f
drewplette <60942408+drewplette@users.noreply.github.com>
2023-01-22 23:50:54 -0600

    Initial commit of DesireCalc

---
+++ DesireCalc/Form1.cs
@@ -0,0 +1,15 @@
+namespace DesireCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelArmor_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
